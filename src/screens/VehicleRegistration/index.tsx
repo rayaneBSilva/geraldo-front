ddb36e09b2d6a80dcd821dde906e6d76e6df7c2b
igoramf <igoraaf1@gmail.com>
2024-03-13 18:44:13 -0300

    feat: yupSchemas file

--- src/screens/VehicleRegistration/index.tsx
+++ src/screens/VehicleRegistration/index.tsx
@@ -5,7 +5,7 @@ import CustomButton from '../../components/button';
 import ControlledTextInput from '../../components/controller/ControlledTextInput';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup"
-import * as yup from "yup"
+import { vehicleSchema } from '../../utils/yupSchemas';
 
 type VehicleForm = {
   placa: string,
@@ -15,30 +15,12 @@ type VehicleForm = {
 }
 
 
-const formSchema = yup.object({
-  placa: yup.string()
-    .required('Por favor, informe a placa do veículo.')
-    .matches(/^[A-Za-z]{3}\d{4}$/, 'Por favor, informe uma placa válida no formato AAA1234.'),
-  anoDeFabricação: yup.number()
-    .required('Por favor, informe o ano de fabricação do veículo.')
-    .typeError('Por favor, informe o ano de fabricação do veículo.')
-    .test('valida-ano', 'O ano de fabricação não pode ser maior que o ano seguinte e deve ser maior que 1900.', function (value) {
-      const thisYear = new Date().getFullYear() + 1;
-      return value <= thisYear && value >= 1900;
-    }),
-  modelo: yup.string().required('Por favor, informe o modelo do veículo.'),
-  quilometragemAtual: yup.number()
-    .required('Por favor, informe a quilometragem atual do veículo.')
-    .typeError('Por favor, informe a quilometragem atual do veículo.')
-    .min(0, 'A quilometragem atual não pode ser menor que 0.')
-});
-
 
 function VehicleRegistration() {
 
   const {control, handleSubmit, formState: { errors } } = useForm<VehicleForm>({
     mode: 'onChange',
-    resolver: yupResolver(formSchema)
+    resolver: yupResolver(vehicleSchema)
   })
 
   return (

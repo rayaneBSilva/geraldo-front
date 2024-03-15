430c184df1e436d846474158922fc55ac92ac2be
Rodrigo Rodrigues <rodrigopgrodrigues@outlook.com>
2024-03-15 14:56:17 -0300

    feat: add animation to view car screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import Routes from "./src/routes";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light" />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,10 +14,10 @@ function Routes() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="ViewCarScreen" component={ViewCar} />
+        <Stack.Screen name="CreateEstablishment" component={CreateEstablishment} />
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen}/>
-        <Stack.Screen name="CreateEstablishment" component={CreateEstablishment} />
       </Stack.Navigator>
     </NavigationContainer>
   );

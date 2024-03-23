26eb34f637b771df02cf460c324043104bce96ad
yallerocha <yalle.silva@ccc.ufcg.edu.br>
2024-03-23 19:45:02 -0300

    .

---
+++ src/screens/Mapa/MapaProvisorio.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import UpdateMileageModal from "../../components/updateMileageModal";
+
+export default function App() {
+  const [modalVisible, setModalVisible] = useState<boolean>(true); 
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <UpdateMileageModal isVisible={modalVisible} onClose={closeModal} idVeiculo="1" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
